210378af6fe54f1eb3e22bdd552eed00f329507f
moyangzhan <moyangzhan@qq.com>
2021-05-14 00:42:03 +0800

    增加新的限流方法

--- src/throttle/throttle.js
+++ src/throttle/throttle.js
@@ -1,3 +1,4 @@
+//使用setTimeout
 function throttle(bizFunc, restTime) {
   let resting = false;
   return function (...args) {
@@ -9,5 +10,17 @@ function throttle(bizFunc, restTime) {
       bizFunc.call(this, ...args);
       resting = false;
     }, restTime);
+  };
+}
+
+//使用时间间隔
+function throttle2(bizFunc, restTime) {
+  let lastTime = new Date().getTime();
+  return function (...args) {
+    let now = new Date().getTime();
+    if (now - lastTime >= restTime) {
+      lastTime = now;
+      bizFunc.call(this, ...args);
+    }
   };
 }
